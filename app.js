e57f9b22b26972d6051daad2a779e669541f92e3
Chandan Kumar <chandankrr.91@gmail.com>
2023-09-11 11:46:51 +0530

    implemented signup functionaly

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const ErrorHandler = require('./utils/ErrorHandler');
 const app = express();
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
+app.use('/', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload({ useTempFiles: true }));
 
 // config
 if (process.env.NODE_ENV !== 'production') {
@@ -16,6 +17,10 @@ if (process.env.NODE_ENV !== 'production') {
     path: 'config/.env',
   });
 }
+
+// import routes
+const user = require('./controller/user');
+app.use('/api/v2/user', user);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
