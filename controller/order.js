718253d45c2f03b6febeaebfcddcba34174ad363
Chandan Kumar <chandankrr.91@gmail.com>
2023-11-20 11:46:02 +0530

    added routes for all orders for admin

--- controller/order.js
+++ controller/order.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ErrorHandler = require('../utils/ErrorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
-const { isSeller } = require('../middleware/auth');
+const { isAuthenticated, isSeller, isAdmin } = require('../middleware/auth');
 const Order = require('../model/order');
+const Shop = require('../model/shop');
 const Product = require('../model/product');
 
 // create new order
@@ -95,7 +96,6 @@ router.put(
   catchAsyncErrors(async (req, res, next) => {
     try {
       const order = await Order.findById(req.params.id);
-
       if (!order) {
         return next(new ErrorHandler('Order not found with this id', 400));
       }
@@ -104,28 +104,33 @@ router.put(
           await updateOrder(o._id, o.qty);
         });
       }
-
       order.status = req.body.status;
-
       if (req.body.status === 'Delivered') {
         order.deliveredAt = Date.now();
         order.paymentInfo.status = 'Succeeded';
+        const serviceCharge = order.totalPrice * 0.1;
+        await updateSellerInfo(order.totalPrice - serviceCharge);
       }
 
       await order.save({ validateBeforeSave: false });
-
       res.status(200).json({
         success: true,
         order,
       });
-
       async function updateOrder(id, qty) {
         const product = await Product.findById(id);
-
         product.stock -= qty;
         product.sold_out += qty;
 
         await product.save({ validateBeforeSave: false });
+      }
+
+      async function updateSellerInfo(amount) {
+        const seller = await Shop.findById(req.seller.id);
+
+        seller.availableBalance = amount;
+
+        await seller.save();
       }
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
@@ -166,29 +171,22 @@ router.put(
   catchAsyncErrors(async (req, res, next) => {
     try {
       const order = await Order.findById(req.params.id);
-
       if (!order) {
         return next(new ErrorHandler('Order not found with this id', 400));
       }
-
       order.status = req.body.status;
-
       await order.save();
-
       res.status(200).json({
         success: true,
         message: 'Order Refund successfull!',
       });
-
       if (req.body.status === 'Refund Success') {
         order.cart.forEach(async (o) => {
           await updateOrder(o._id, o.qty);
         });
       }
-
       async function updateOrder(id, qty) {
         const product = await Product.findById(id);
-
         product.stock += qty;
         product.sold_out -= qty;
 
@@ -199,4 +197,26 @@ router.put(
     }
   })
 );
+
+// all orders --- for admin
+router.get(
+  '/admin-all-orders',
+  isAuthenticated,
+  isAdmin('Admin'),
+  catchAsyncErrors(async (req, res, next) => {
+    try {
+      const orders = await Order.find().sort({
+        deliveredAt: -1,
+        createdAt: -1,
+      });
+      res.status(201).json({
+        success: true,
+        orders,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 module.exports = router;
